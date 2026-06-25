dd53fb42e2a6352e74d93a71781a41fc705db993
jewoodev <jewoos15@naver.com>
2026-06-25 16:09:32 +0900

    chore: scaffold spring boot backend
    Signed-off-by: jewoodev <jewoos15@naver.com>

---
+++ backend/src/main/java/com/giservice/busmonitoring/BusMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.giservice.busmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusMonitoringApplication.class, args);
+	}
+
+}
